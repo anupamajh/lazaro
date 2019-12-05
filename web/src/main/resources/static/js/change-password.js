076807d7755d475e7ce83890fd75f7e9f91e1782
Tasmiabanu <tasmia@carmelsolutions.net>
2019-12-05 12:52:02 +0530

    java script for change password with some changes in css

--- web/src/main/resources/static/js/change-password.js
+++ web/src/main/resources/static/js/change-password.js
@@ -1,29 +1,100 @@
  function ToggleCurrentPassword() {
         var temp = document.getElementById("newEmail1");
-        if (temp.type === "password") {
-            temp.type = "text";
-        } else {
-            temp.type = "password";
-        }
+                              if (temp.type === "password") {
+                                  temp.type = "text";
+                                  document.getElementById("passwordMask1").style.display = "block";
+      //                            document.getElementById("passwordUnmask").style.bottom = "38%";
+      document.getElementById("passwordUnmask1").style.display = "none";
+                                  document.getElementById("passwordMask1").style.bottom = "37%";
+                              }
+                              else {
+                                  temp.type = "password";
+                                  document.getElementById("passwordMask1").style.display = "block";
+        document.getElementById("passwordMask1").style.bottom = "38%";
+        document.getElementById("passwordUnmask1").style.display = "none";
+                              }
     }
+function mask1() {
+  var temp = document.getElementById("newEmail1");
+                         if (temp.type === "password") {
+                             temp.type = "text";
+                             document.getElementById("passwordMask1").style.display = "none";
+                               document.getElementById("passwordUnmask1").style.display = "block";
 
+                         }
+                         else {
+                             temp.type = "password";
+                             document.getElementById("passwordMask1").style.display = "none";
+                              document.getElementById("passwordUnmask1").style.display = "block";
+                               document.getElementById("passwordMask1").style.bottom = "38%"
+                         }}
     function ToggleNewPassword() {
         var temp = document.getElementById("newEmail2");
-        if (temp.type === "password") {
-            temp.type = "text";
-        } else {
-            temp.type = "password";
-        }
+                                     if (temp.type === "password") {
+                                         temp.type = "text";
+                                         document.getElementById("passwordMask2").style.display = "block";
+             //                            document.getElementById("passwordUnmask").style.bottom = "38%";
+             document.getElementById("passwordUnmask2").style.display = "none";
+                                         document.getElementById("passwordMask2").style.bottom = "37%";
+                                     }
+                                     else {
+                                         temp.type = "password";
+                                         document.getElementById("passwordMask2").style.display = "block";
+               document.getElementById("passwordMask2").style.bottom = "38%";
+               document.getElementById("passwordUnmask2").style.display = "none";
+                                     }
     }
+function mask2() {
+  var temp = document.getElementById("newEmail2");
+                         if (temp.type === "password") {
+                             temp.type = "text";
+                             document.getElementById("passwordMask2").style.display = "none";
+                               document.getElementById("passwordUnmask2").style.display = "block";
+
+                         }
+                         else {
+                             temp.type = "password";
+                             document.getElementById("passwordMask2").style.display = "none";
+                              document.getElementById("passwordUnmask2").style.display = "block";
+                               document.getElementById("passwordMask2").style.bottom = "38%"
+                         }}
 
     function ToggleConfirmPassword() {
         var temp = document.getElementById("newEmail3");
-        if (temp.type === "password") {
-            temp.type = "text";
-        } else {
-            temp.type = "password";
-        }
+                                     if (temp.type === "password") {
+                                         temp.type = "text";
+                                         document.getElementById("passwordMask3").style.display = "block";
+             //                            document.getElementById("passwordUnmask").style.bottom = "38%";
+             document.getElementById("passwordUnmask3").style.display = "none";
+                                         document.getElementById("passwordMask3").style.bottom = "37%";
+                                     }
+                                     else {
+                                         temp.type = "password";
+                                         document.getElementById("passwordMask3").style.display = "block";
+               document.getElementById("passwordMask3").style.bottom = "38%";
+               document.getElementById("passwordUnmask3").style.display = "none";
+                                     }
     }
+function mask3() {
+  var temp = document.getElementById("newEmail3");
+                         if (temp.type === "password") {
+                             temp.type = "text";
+                             document.getElementById("passwordMask3").style.display = "none";
+                               document.getElementById("passwordUnmask3").style.display = "block";
+
+                         }
+                         else {
+                             temp.type = "password";
+                             document.getElementById("passwordMask3").style.display = "none";
+                              document.getElementById("passwordUnmask3").style.display = "block";
+                               document.getElementById("passwordMask3").style.bottom = "38%"
+                         }}
+
+
+
+
+
+
 
     function ChangePasswordSuccess() {
         location.replace("/change-password/change-password");
