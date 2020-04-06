ca8b5c22ff42ecaadee69ca286f4cba8ffe94c46
Prasanna Kumar pete <prasanna.pete@gmail.com>
2020-04-06 06:20:12 +0530

    Data Migration Initial commit

---
+++ spring/data-migrator/src/main/java/com/carmel/common/datamigrator/DataMigratorApplication.java
@@ -0,0 +1,13 @@
+package com.carmel.common.datamigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMigratorApplication.class, args);
+	}
+
+}
---
+++ spring/data-migrator/src/test/java/com/carmel/common/datamigrator/DataMigratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmel.common.datamigrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
