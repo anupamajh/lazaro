dfbaf813bf67d2ac8e324d7921973083b8ccf227
ronaldphilip <ronald@carmelsolutions.net>
2020-10-28 20:05:51 +0530

    Additional Ticket Status added

--- spring/guestjini-service/src/main/java/com/carmel/guestjini/service/common/HelpDesk/TicketStatus.java
+++ spring/guestjini-service/src/main/java/com/carmel/guestjini/service/common/HelpDesk/TicketStatus.java
@@ -1,9 +1,11 @@
 package com.carmel.guestjini.service.common.HelpDesk;
 
 public abstract class TicketStatus {
-    public final static int  DRAFT = 0;
-    public final static int  COMPLETED = 1;
-    public final static int  WORK_IN_PROGRESS = 2;
-    public final static int  NOT_STARTED = 3;
+    public final static int  DRAFT = 0; // Draft
+    public final static int  COMPLETED = 1; // Active
+    public final static int  WORK_IN_PROGRESS = 2; // Active
+    public final static int  NOT_STARTED = 3; // Active
+    public final static int ON_HOLD = 4; // Active
+    public final static int CLOSED = 5; // Archive
 
 }
